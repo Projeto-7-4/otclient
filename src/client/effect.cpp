cf7d7e24f0b01b6767696833ce76987a1a9fb9db
Bruno <seu-email@exemplo.com>
2025-11-22 19:00:49 +0000

    🔧 Restaurar effect.cpp do branch main (versão original que funcionava)
    - Restaura código do branch main atual
    - Remove todas as modificações do efeito 173
    - Efeito 173 deve funcionar normalmente

--- src/client/effect.cpp
+++ src/client/effect.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2014 OTClient <https://github.com/edubart/otclient>
+ * Copyright (c) 2010-2025 OTClient <https://github.com/edubart/otclient>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,56 +21,153 @@
  */
 
 #include "effect.h"
-#include "map.h"
-#include <framework/core/eventdispatcher.h>
 
-void Effect::draw(const Point& dest, float scaleFactor, bool animate, int offsetX, int offsetY, LightView *lightView)
+#include "animator.h"
+#include "client.h"
+#include "game.h"
+#include "gameconfig.h"
+#include "map.h"
+#include "thingtype.h"
+#include "thingtypemanager.h"
+#include "framework/core/eventdispatcher.h"
+#include "framework/graphics/drawpoolmanager.h"
+#include "framework/graphics/shadermanager.h"
+
+void Effect::draw(const Point& dest, const bool drawThings, LightView* lightView)
 {
-    if(m_id == 0)
+    if (!canDraw() || isHided())
+        return;
+
+    // It only starts to draw when the first effect as it is about to end.
+    if (m_animationTimer.ticksElapsed() < m_timeToStartDrawing)
         return;
 
     int animationPhase = 0;
-    if(animate)
-        animationPhase = std::min<int>((int)(m_animationTimer.ticksElapsed() / m_phaseDuration), getAnimationPhases() - 1);
+    if (canAnimate()) {
+        if (g_game.getFeature(Otc::GameEnhancedAnimations)) {
+            const auto* animator = getThingType()->getIdleAnimator();
+            if (!animator)
+                return;
 
-    int xPattern = offsetX % getNumPatternX();
-    if(xPattern < 0)
-        xPattern += getNumPatternX();
+            // This requires a separate getPhaseAt method as using getPhase would make all magic effects use the same phase regardless of their appearance time
+            animationPhase = animator->getPhaseAt(m_animationTimer);
+        } else {
+            // hack to fix some animation phases duration, currently there is no better solution
+            int ticks = g_gameConfig.getEffectTicksPerFrame();
+            if (m_clientId == 33) {
+                ticks <<= 2;
+            }
 
-    int yPattern = offsetY % getNumPatternY();
-    if(yPattern < 0)
-        yPattern += getNumPatternY();
+            animationPhase = std::min<int>(static_cast<int>(m_animationTimer.ticksElapsed() / ticks), getAnimationPhases() - 1);
+        }
+    }
 
-    rawGetThingType()->draw(dest, scaleFactor, 0, xPattern, yPattern, 0, animationPhase, lightView);
+    const int offsetX = m_position.x - g_map.getCentralPosition().x;
+    const int offsetY = m_position.y - g_map.getCentralPosition().y;
+
+    int xPattern = static_cast<unsigned>(offsetX) % getNumPatternX();
+    xPattern = 1 - xPattern - getNumPatternX();
+    if (xPattern < 0) xPattern += getNumPatternX();
+
+    int yPattern = static_cast<unsigned>(offsetY) % getNumPatternY();
+
+    if (g_game.getFeature(Otc::GameMapOldEffectRendering)) {
+        xPattern = offsetX % getNumPatternX();
+        if (xPattern < 0)
+            xPattern += getNumPatternX();
+
+        yPattern = offsetY % getNumPatternY();
+        if (yPattern < 0)
+            yPattern += getNumPatternY();
+    }
+
+    if (g_drawPool.getCurrentType() == DrawPoolType::MAP) {
+        if (drawThings && g_client.getEffectAlpha() < 1.f)
+            g_drawPool.setOpacity(g_client.getEffectAlpha(), true);
+    }
+
+    if (hasShader())
+        g_drawPool.setShaderProgram(g_shaders.getShaderById(m_shaderId), true/*, shaderAction*/);
+
+    // Effect 173 (Critical Damage) uses 64x64 sprites instead of 32x32
+    const uint8_t EFFECT_CRITICAL_DAMAGE = 173;
+    if (m_clientId == EFFECT_CRITICAL_DAMAGE) {
+        // Temporarily set scale factor to 2.0 for 64x64 sprites
+        const float originalScale = g_drawPool.getScaleFactor();
+        g_drawPool.setScaleFactor(originalScale * 2.0f);
+        const Point scaledDest = dest - Point(g_gameConfig.getSpriteSize() / 2);
+        getThingType()->draw(scaledDest, 0, xPattern, yPattern, 0, animationPhase, Color::white, drawThings, lightView);
+        g_drawPool.setScaleFactor(originalScale);
+    } else {
+        getThingType()->draw(dest, 0, xPattern, yPattern, 0, animationPhase, Color::white, drawThings, lightView);
+    }
 }
 
 void Effect::onAppear()
 {
-    m_animationTimer.restart();
-    m_phaseDuration = EFFECT_TICKS_PER_FRAME;
+    if (g_game.getFeature(Otc::GameEnhancedAnimations)) {
+        const auto* animator = getThingType()->getIdleAnimator();
+        if (!animator)
+            return;
 
-    // hack to fix some animation phases duration, currently there is no better solution
-    if(m_id == 33)
-        m_phaseDuration <<= 2;
+        m_duration = animator->getTotalDuration();
+    } else {
+        m_duration = g_gameConfig.getEffectTicksPerFrame();
+
+        // hack to fix some animation phases duration, currently there is no better solution
+        if (m_clientId == 33) {
+            m_duration <<= 2;
+        }
+
+        m_duration *= getAnimationPhases();
+    }
+
+    m_animationTimer.restart();
 
     // schedule removal
-    auto self = asEffect();
-    g_dispatcher.scheduleEvent([self]() { g_map.removeThing(self); }, m_phaseDuration * getAnimationPhases());
+    g_dispatcher.scheduleEvent([self = asEffect()] { g_map.removeThing(self); }, m_duration);
 }
 
-void Effect::setId(uint32 id)
+bool Effect::waitFor(const EffectPtr& effect)
 {
-    if(!g_things.isValidDatId(id, ThingCategoryEffect))
-        id = 0;
-    m_id = id;
+    const ticks_t ticksElapsed = effect->m_animationTimer.ticksElapsed();
+    uint16_t minDuration = getIdleAnimator() ? getIdleAnimator()->getMinDuration() : g_gameConfig.getEffectTicksPerFrame();
+    minDuration = minDuration * std::max<uint8_t>(getAnimationPhases() / 3, 1);
+
+    if (ticksElapsed <= minDuration)
+        return false;
+
+    const int duration = effect->m_duration / (g_app.mustOptimize() || g_app.isForcedEffectOptimization() ? 1.5 : 3);
+    m_timeToStartDrawing = std::max<int>(0, duration - effect->m_animationTimer.ticksElapsed());
+
+    return true;
 }
 
-const ThingTypePtr& Effect::getThingType()
+void Effect::setId(const uint32_t id)
 {
-    return g_things.getThingType(m_id, ThingCategoryEffect);
+    if (!g_things.isValidDatId(id, ThingCategoryEffect))
+        return;
+
+    m_clientId = id;
 }
 
-ThingType *Effect::rawGetThingType()
+void Effect::setPosition(const Position& position, const uint8_t stackPos)
 {
-    return g_things.rawGetThingType(m_id, ThingCategoryEffect);
+    if (m_clientId == 0)
+        return;
+
+    Thing::setPosition(position, stackPos);
+    int pattern_x = getNumPatternX();
+    int pattern_y = getNumPatternY();
+    if (pattern_x == 0 || pattern_y == 0) {
+        g_logger.warning("Failed to send magic effect {}. No sprites declared.", m_clientId);
+        return;
+    }
+
+    m_numPatternX = m_position.x % pattern_x;
+    m_numPatternY = m_position.y % pattern_y;
+}
+
+ThingType* Effect::getThingType() const {
+    return g_things.getRawThingType(m_clientId, ThingCategoryEffect);
 }
